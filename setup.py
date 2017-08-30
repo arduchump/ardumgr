f8b4ab26fb1e98e613ef847c47a60f71fe985c28
Hong-She Liang <starofrainnight@gmail.com>
2017-08-30 16:02:01 +0800

    Depends on rabird.core at least 3.1.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file, open('HISTORY.rst') as history_file:
 
 requirements = [
     'click>=6.0',
-    'rabird.core>=3.1.2',
+    'rabird.core>=3.1.3',
 ]
 
 setup_requirements = [
